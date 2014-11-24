af77fd207711d519f2db61e8d175a3310433c1df
Miroufff <sylvain.davenel@laposte.net>
2014-11-24 16:25:05 +0100

    Fonction Trouver ----- UtilisateurDAO

--- src/fr/iia/DAO/UtilisateurDAO.java
+++ src/fr/iia/DAO/UtilisateurDAO.java
@@ -6,6 +6,7 @@
 package fr.iia.DAO;
 
 import fr.iia.Class.Adresse;
+import fr.iia.Class.Utilisateur;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,23 +17,28 @@ import java.sql.Statement;
  * @author Enzo
  */
 public class UtilisateurDAO {
-    public static Utilisateur trouver(Connection cnx, String nom, String prenom){
-		Utilisateur Utilisateur = null;
+    public static Utilisateur trouver(Connection cnx, String nom_artiste){
+		Utilisateur utilisateur = null;
 		Statement stmt = null;
 		try{			
 			stmt = cnx.createStatement();
-			ResultSet rs = stmt.executeQuery("Select id, age, idAdr From Personne WHERE nom = '" + nom + "' AND prenom = '" + prenom + "';");
+			ResultSet rs = stmt.executeQuery("Select nom, prenom, idAdr From Personne WHERE nom_artiste = '" + nom_artiste + "';");
 			if(rs.next()){
 				int idAdr = rs.getInt("idAdr");
 				
 				Adresse adresse = AdresseDAO.trouver(cnx , idAdr);
+                                String login = rs.getString("login");
+                                String password = rs.getString("password");
+                                String nom = rs.getString("nom");
+                                String prenom = rs.getString("prenom");
+                                String mail = rs.getString("mail");
+                                String telephone = rs.getString("telephone");
+                                String descr_util = rs.getString("descr_util");
 				int age = rs.getInt("age");
 				int id = rs.getInt("id");
 				
-				utilisateur = new Utilisateur(nom, prenom, age, adresse);
-				//personne.setAge(age);
+				utilisateur = new Utilisateur(login, password, nom, prenom, nom_artiste, age, mail, telephone, adresse, descr_util);
 				utilisateur.setId(id);
-				
 			}
 			
 		}catch(Exception ex){
@@ -50,6 +56,6 @@ public class UtilisateurDAO {
 		
 		
 		
-		return personne;
+		return utilisateur;
 	}
 }
