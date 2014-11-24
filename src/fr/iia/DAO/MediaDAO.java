459a753d9c7163a73eae0f405e1d5cb65b3c79e6
MegaProjet <hardy.k@neuf.fr>
2014-11-24 16:27:46 +0100

    Creer MediaDAO

--- src/fr/iia/DAO/MediaDAO.java
+++ src/fr/iia/DAO/MediaDAO.java
@@ -5,10 +5,51 @@
  */
 package fr.iia.DAO;
 
+import fr.iia.Class.Media;
+import fr.iia.Class.TypeMedia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Enzo
  */
 public class MediaDAO {
     
+    public static void creer(Connection cnx, Media med){
+			
+		PreparedStatement pstmt = null;
+		try{
+			pstmt = cnx.prepareStatement("INSERT INTO media"
+                                                    + " (nom, taille, id_type)"
+                                                    + " VALUES (?, ?)");
+				
+			pstmt.setString(1, med.getNom() );
+			pstmt.setInt(2, med.getTaille());
+						
+			pstmt.executeUpdate();
+			
+			ResultSet rs = pstmt.executeQuery("SELECT MAX(id) FROM media");
+			if(rs.next()){
+				int id = rs.getInt(1);
+				med.setId(id);
+			}
+			
+			System.out.println("Création réussie.");
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+			System.out.println("Création échouée.");
+		}finally{
+			if(pstmt != null){
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
